cbf298b71b28d822f0fd01c33de12cc287966f1e
hallgató <hallgat@LABOR115-02>
2017-10-04 09:04:40 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    { 
+     static int[] generateArray()
+        {
+
+            int[] arr = new int[n];
+            Random R = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int v = R.Next(0, 100);
+                if (v < 40) arr[i] = R.Next(1, 11); //Land
+                else arr[i] = 0; //sea  
+            } //end for
+            return arr;
+            } //
+        // returns how often value accurs in array
+        Statc int getNum(int[] array, int value)
+        {
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+
+            {
+                if array [i] ==
+            }
+        }
+        static int getMax(int[])
+        static void Main(string[] args)
+        {
+
+            const int n = 10;
+            Console.WriteLine("Task 1: generate heights (0=sea, >0 land");
+            int[] array = generateArray(n);
+            array[S] = 10;
+            print(array);
+            Console.WriteLine("Task 2: max index:" + maxindex +
+                " max height: );
+                Console.WriteLine("Task 3: How often is the highest peak attained? ");
+            Console.WriteLine(getNume(array, array[maxindex]))
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+                Console.ReadKey();
+                  }
+    
+    
+    }
+}
